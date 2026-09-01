dc0909b9e2287c8a0d85b7fa6f4d28962811f3a2
Pavel Yelinevich <pashaelinevitch@yandex.com>
2026-09-01 22:23:37 +0300

    review

---
+++ src/components/app/app.tsx
@@ -0,0 +1,33 @@
+import { CSSProperties, useState } from 'react';
+import clsx from 'clsx';
+
+import { Article } from '../article/Article';
+import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
+
+import styles from './app.module.scss';
+
+export const App = () => {
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--bg-color': articleState.backgroundColor.value,
+					'--container-width': articleState.contentWidth.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm onApply={setArticleState} />
+			<Article />
+		</main>
+	);
+};
